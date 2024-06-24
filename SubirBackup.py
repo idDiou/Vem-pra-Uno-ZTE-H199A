8519b4e93e204f9f80c8a35712492207771d5f70
Dio Apenas <Dionatarogerio@hotmail.com>
2024-06-24 02:24:57 -0300

    level 4

--- SubirBackup.py
+++ SubirBackup.py
@@ -3,9 +3,18 @@ import time
 import pyperclip
 import keyboard
 import os
+import threading
 
 #intervalo entre os comandos
 pa.PAUSE = 1
+
+#Para poder interromper o código
+interromper = False
+def monitorar_interrupcao():
+    global interromper
+    keyboard.wait('esc')
+    interromper = True
+    print("Interrupção solicitada pelo usuário. Finalizando...")
 
 #função principal/inicial
 def main():
@@ -16,7 +25,7 @@ def main():
 
 #Verificação
 def verificar():
-    while True:
+    while not interromper:
         print('1-Roteador Novo da Caixa')
         print('2-Roteador da Rua')
         try:
@@ -25,19 +34,12 @@ def verificar():
                 print('aguarde alguns instantes')
                 Acesso()
                 RoteadorF()
-                novo()
-                gerenciamento()
-                backup()
-                Cbackup()
                 break
             
             elif veri == 2:
                 print('aguarde alguns instantes')
                 Acesso()
                 RoteadorRua()
-                gerenciamento()
-                backup()
-                Cbackup()
                 break
             
             else:
@@ -47,39 +49,53 @@ def verificar():
             os.system('cls')
             print('Entrada inválida. Digite apenas números.')
             print('digite enter para continuar')
-            keyboard.wait('any')
-
+            keyboard.wait('enter')
+        if interromper:
+            break
 
 #acessa o navegador
 def Acesso():
+    if interromper:
+        return
     pa.press('Win')
     pa.write('edge')
     pa.press('enter')
 
 #acessa o roteador caso seja de fábrica
-def RoteadorF():    
+def RoteadorF():  
+    if interromper:
+        return  
     pa.write('192.168.1.1')
     pa.press('enter')
     pa.write('multipro')
     pa.press('tab')
     pa.write('multipro')
     pa.press('enter')
+    novo()
 
 def RoteadorRua():
+    if interromper:
+        return
     pa.write('192.168.1.1')
     pa.press('enter')
     pa.write('multipro')
     pa.press('tab')
     pa.write('Vpu@2018')
     pa.press('enter')
+    gerenciamento()
 
 #remove o assistente
 def novo():
+    if interromper:
+        return
     pa.press('tab', 6)
     pa.press('enter')
+    gerenciamento()
 
 #acessa o gerenciamento 
 def gerenciamento():
+    if interromper:
+        return
     pa.press('enter')
     pa.hold('win')
     pa.press('up')
@@ -93,9 +109,12 @@ def gerenciamento():
     time.sleep(1)
     pa.mouseUp(button='left')
     pa.click(x=1531, y=223)
+    backup()
 
 #acessa o backup
 def backup():
+    if interromper:
+        return
     pa.click(x=663, y=696)
     pa.press('tab')
     pa.press('enter')
@@ -104,16 +123,23 @@ def backup():
     pa.write('C:\\Users\\diona\\AppData\\Local\\Programs\\Python\\Python311\\Projetos\\Automatizar H199A')
     pa.press('enter')
     pa.press('tab', 6)
-    pa.write('defaut')
+    pa.write('default')
     pa.press('enter')
+    Cbackup()
 
 #confirma o backup
 def Cbackup():
+    if interromper:
+        return
     pa.press('tab')
     pa.press('enter')
     pa.press('tab')
     pa.press('enter')
 
 if __name__ == "__main__":
+    # Inicia o monitoramento da tecla de interrupção em uma thread separada
+    interrupcao_thread = threading.Thread(target=monitorar_interrupcao)
+    interrupcao_thread.daemon = True
+    interrupcao_thread.start()
+    
     main()
-
--- Subirpresset.py
+++ Subirpresset.py
@@ -3,5 +3,91 @@ import time
 import pyperclip
 import keyboard
 import os
-import SubirBackup
+import threading
 
+#intervalo entre os comandos
+pa.PAUSE = 1
+
+#Para poder interromper o código
+interromper = False
+def monitorar_interrupcao():
+    global interromper
+    keyboard.wait('esc')
+    interromper = True
+    print("Interrupção solicitada pelo usuário. Finalizando...")
+
+def main():
+    global MAC
+    print('-----Bem Vindo ao Automatizador VPU-----')
+    MAC = input('por gentileza digite o mac: ')
+    print('o mac é:', MAC)    
+    acesso()
+
+#acessa o navegador
+def acesso():
+    if interromper:
+        return
+    pa.press('Win')
+    pa.write('edge')
+    pa.press('enter')
+    RoteadorRua()
+
+#Loga no roteador
+def RoteadorRua():
+    if interromper:
+        return
+    pa.write('192.168.1.1')
+    pa.press('enter')
+    pa.write('multipro')
+    pa.press('tab')
+    pa.write('Vpu@2018')
+    pa.press('enter')
+    wan()
+
+#acessa a Wan e altera o ppoe
+def wan():
+    if interromper:
+        return
+    pa.press('tab', 3)
+    pa.press('enter')
+    pa.press('tab', 7)
+    pa.press('enter')
+    pa.press('f11')
+    pa.click(x=659, y=488)
+    pa.press('tab', 8)
+    pa.write(MAC[-6:] + '@vemprauno')
+    lan()
+
+#acessa o wifi e senha
+def lan():
+    if interromper:
+        return
+    pa.press('tab', 11)
+    pa.press('enter')
+    pa.press('home')
+    pa.press('tab', 9)
+    pa.press('enter')
+    pa.press('tab', 7)
+    pa.press('enter')
+    pa.click(x=724, y=969)
+    pa.press('tab', 3)
+    g4()
+
+#usuario e senha do 4g
+def g4():
+    pa.write('VempraUno_' + MAC[-4:])
+    pa.press('tab', 3)
+    pa.write(MAC[-8:])
+    pa.press('tab', 4)
+    pa.press('enter')
+    pa.press('tab')
+    pa.press('enter')
+    
+
+if __name__ == "__main__":
+    # Inicia o monitoramento da tecla de interrupção em uma thread separada
+    interrupcao_thread = threading.Thread(target=monitorar_interrupcao)
+    interrupcao_thread.daemon = True
+    interrupcao_thread.start()
+    
+    main()
